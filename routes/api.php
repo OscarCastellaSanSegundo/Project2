1cae5415b9121666bf8b977d12b15b35379e1f7d
ogonzalezz <ogonzalezz27@cepnet.net>
2022-03-17 13:50:48 +0100

    apis

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
-use App\Http\Controllers\Api\MapController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MapController;
+use App\Http\Controllers\Api\PerfilController;
+use App\Http\Controllers\Api\UsuariController;
+use App\Http\Controllers\Api\AgenciaController;
+use App\Http\Controllers\Api\ComarcaController;
+use App\Http\Controllers\Api\IncidentController;
+use App\Http\Controllers\Api\MunicipiController;
+use App\Http\Controllers\Api\ExpedientController;
+use App\Http\Controllers\Api\ProvinciaController;
+use App\Http\Controllers\Api\CartaTrucadaController;
+use App\Http\Controllers\Api\EstatAgenciaController;
+use App\Http\Controllers\Api\TipusIncidentController;
+use App\Http\Controllers\Api\DadesPersonalsController;
+use App\Http\Controllers\Api\EstatExpedientController;
+use App\Http\Controllers\Api\TipusLocalitzacioController;
+use App\Http\Controllers\Api\CartesTrucadesHasAgenciesController;
 
 /*
 |--------------------------------------------------------------------------
