d02edd3158f6597527ffaa2fd70d7514534767b0
OscarCastella-Examen <oscarcastella@hotmail.com>
2022-03-31 13:56:54 +0200

    añadido menu admin

--- resources/views/estadisticas/index.blade.php
+++ resources/views/estadisticas/index.blade.php
@@ -32,15 +32,22 @@ Expedients
 @endsection
 
 @section('contenido')
-
-<div class="principalEstadisticas">
-    <div>
-        <canvas id="myChart2"></canvas>
-    </div>
-    <div>
-        <canvas id="myChart"></canvas>
+<div class="generalEstadisticas">
+    <div class="principalEstadisticas">
+        <div class="grafico2i3">
+            <div>
+                <canvas id="myChart2" class="grafico1"></canvas>
+            </div>
+            <div>
+                <canvas id="myChart3" class="grafico1"></canvas>
+            </div>
+        </div>
+        <div>
+            <canvas id="myChart" class="grafico2"></canvas>
+        </div>
     </div>
 </div>
+
 
 
 <script>
@@ -133,6 +140,61 @@ Expedients
     );
 
 
+
+    const data3 = {
+    labels: [
+            'Accidents',
+            'Assistència sanitària',
+            'Incendi',
+            'Fuita (Aigua, gas, altres)',
+            'Altres incidències',
+            'Seguretat',
+            'Trànsit',
+            'Civisme',
+            'Medi ambient',
+            'Meteorologia'
+        ],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40, 30, 20, 10],
+        backgroundColor: [
+        'rgba(92, 67, 14, 0.2)',
+        'rgba(0, 0, 255, 0.2)',
+        'rgba(255, 0, 0, 0.2)',
+        'rgba(56, 113, 187, 0.2)', //
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+        'rgb(92, 67, 14, 0.8)',
+        'rgb(0, 0, 255, 0.8)',
+        'rgb(255, 0, 0, 0.8)',
+        'rgb(56, 113, 187, 0.8)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+    }]
+    };
+
+    const config3 = {
+        type: 'bar',
+        data: data3,
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
+        }
+        },
+    };
+
+    const myChart3 = new Chart(
+      document.getElementById('myChart3'),
+      config3
+    );
 
 
 
