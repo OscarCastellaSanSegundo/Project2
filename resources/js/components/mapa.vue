f5a0f79c1386b9e4ec4176a5f81ff6ad14d2feaf
Borja <borja.garcia@babelgroup.com>
2022-04-25 12:08:26 +0200

    .csss

--- resources/js/components/mapa.vue
+++ resources/js/components/mapa.vue
@@ -6,215 +6,66 @@
 
 <script>
 export default {
-    data() {
-        return {
+    data(){
+        return{
             agencies: [],
-            agenciesRecomanades: [],
+            loading: false,
             map: {},
-            accessToken:
-                "pk.eyJ1IjoiYm9yamFnYXJjaWEiLCJhIjoiY2wyYTh6ZGg4MDFsZzNlb2EzMGVhejdvdCJ9.Zp8aJej_Dctrr88OrwbPrQ"
-        };
+            accessToken:'pk.eyJ1IjoiYm9yamFnYXJjaWEiLCJhIjoiY2wyYTh6ZGg4MDFsZzNlb2EzMGVhejdvdCJ9.Zp8aJej_Dctrr88OrwbPrQ',
+        }
     },
     methods: {
-        markAgencies() {
-            for (const agencia of this.agencies) {
-                this.positionMark(agencia);
-            }
+        selectAgencies(){
+            this.loading = true;
+            let me = this;
+                axios
+                    .get('/agencies')
+                    .then(response => {
+                        me.agencies = response.data;
+                        this.crearMapa('Barcelona, Barcelona', me.agencies);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+                    .finally(() => this.loading = false);
         },
 
-        selectAgencies() {
+        crearMapa(place, agencies){
             let me = this;
-            axios
-                .get("/agencia")
-                .then((result) => {
-                    me.agencies = result.data;
-                    this.positionMarkIncident("Barcelona, Barcelona");
-
-                    this.markAgencies();
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        },
-
-        positionMarkIncident(place) {
-            let me = this;
-            mapboxgl.accessToken = this.accessToken;
-
-            const mapboxClient = mapboxSdk({
-                accessToken: mapboxgl.accessToken,
-            });
+            mapboxgl.accesToken = this.accessToken;
+            const mapboxClient = mapboxSdk({accesToken: mapboxgl.accessToken});
             mapboxClient.geocoding
-                .forwardGeocode({
-                    query: place,
-                    autocomplete: false,
-                    limit: 1,
-                })
-                .send()
-                .then((response) => {
-                    if (
-                        !response ||
-                        !response.body ||
-                        !response.body.features ||
-                        !response.body.features.length
-                    ) {
-                        console.error("Invalid response:");
-                        console.error(response);
-                        return;
-                    }
-
-                    const feature = response.body.features[0];
-
-                    me.map = new mapboxgl.Map({
-                        container: "map",
-                        style: "mapbox://styles/mapbox/streets-v11",
-                        center: feature.center,
-                        zoom: 12,
-                    });
-
-                    // Crea un marcador y lo añade al mapa
-                    new mapboxgl.Marker({
-                        color: "#E74C3C",
-                    })
-                        .setLngLat(feature.center)
-                        .addTo(me.map);
-                });
-        },
-        positionMark(agencia) {
-            let me = this;
-            mapboxgl.accessToken = this.accessToken;
-
-            const mapboxClient = mapboxSdk({
-                accessToken: mapboxgl.accessToken,
-            });
-            mapboxClient.geocoding
-                .forwardGeocode({
-                    query: agencia.carrer + ", " + agencia.municipi.nom,
-                    autocomplete: false,
-                    limit: 1,
-                })
-                .send()
-                .then((response) => {
-                    if (
-                        !response ||
-                        !response.body ||
-                        !response.body.features ||
-                        !response.body.features.length
-                    ) {
-                        console.error("Invalid response:");
-                        console.error(response);
-                        return;
-                    }
-
-                    const feature = response.body.features[0];
-
-                    const marker = new mapboxgl.Marker({
-                        color: "#8E44AD",
-                    });
-
-                    marker.setLngLat(feature.center).addTo(me.map);
-
-                    let div = this.createPopup(
-                        agencia,
-                        marker,
-                        feature,
-                        me.map,
-                        false
-                    );
-
-                    // create the popup
-                    const popup = new mapboxgl.Popup({
-                        offset: 25,
-                    }).setDOMContent(div);
-
-                    marker.setPopup(popup);
-                });
-        },
-        createPopup(agencia, marker, feature, map, recomanat) {
-            let me = this;
-            const pMark = document.createElement("p");
-            pMark.innerText = agencia.nom;
-            pMark.className = "fw-bold";
-            const btnRecomanar = document.createElement("button");
-            btnRecomanar.dataset.recomanat = recomanat;
-            btnRecomanar.dataset.agencia_id = agencia.id;
-            if (recomanat) {
-                btnRecomanar.className = "btn btn-secondary btn-sm";
-                btnRecomanar.innerText = "Treure Recomananació";
-            } else {
-                btnRecomanar.className = "btn btn-primary btn-sm";
-                btnRecomanar.innerText = "Recomanar";
-            }
-
-            btnRecomanar.addEventListener("click", (event) => {
-                let btn = event.target;
-                if (btn.dataset.recomanat == "true") {
-                    me.agenciesRecomanades.splice(
-                        me.agenciesRecomanades.indexOf(btn.dataset.agencia_id),
-                        1
-                    );
-                    marker.remove();
-                    let markernew = new mapboxgl.Marker({
-                        color: "#8E44AD",
-                        rotation: 0,
-                    })
-                        .setLngLat(feature.center)
-                        .addTo(map);
-
-                    let div = me.createPopup(
-                        agencia,
-                        markernew,
-                        feature,
-                        map,
-                        false
-                    );
-                    // create the popup
-                    const popup = new mapboxgl.Popup({
-                        offset: 25,
-                    }).setDOMContent(div);
-
-                    markernew.setPopup(popup);
-                } else {
-                    me.agenciesRecomanades.push(btn.dataset.agencia_id);
-                    console.log(me.agenciesRecomanades);
-                    marker.remove();
-                    let markernew = new mapboxgl.Marker({
-                        color: "#1FC610",
-                        rotation: 45,
-                    })
-                        .setLngLat(feature.center)
-                        .addTo(map);
-
-                    let div = me.createPopup(
-                        agencia,
-                        markernew,
-                        feature,
-                        map,
-                        true
-                    );
-                    // create the popup
-                    const popup = new mapboxgl.Popup({
-                        offset: 25,
-                    }).setDOMContent(div);
-
-                    markernew.setPopup(popup);
+            .forwardGeocode({
+                query: agencies.carrer + ", " + agencies.municipis_id.nom,
+                autocomplete: false,
+                limit: 1
+            })
+            .send()
+            .then((response) => {
+                if (
+                    !response ||
+                    !response.body ||
+                    !response.body.features ||
+                    !response.body.features.length
+                ) {
+                    console.error('Invalid response:');
+                    console.error(response);
+                    return;
                 }
-            });
-            const div = document.createElement("div");
-            div.className = "text-center";
-            div.appendChild(pMark);
-            div.appendChild(btnRecomanar);
 
-            return div;
-        },
-    },
+                const feature = response.body.features[0];
 
-    created() {},
-    mounted() {
-        console.log("Component mounted.");
-        this.selectAgencies();
-    },
-};
+                me.map = new mapboxgl.Map({
+                    container: 'mapa',
+                    style: 'mapbox://styles/mapbox/streets-v11',
+                    center: feature.center,
+                    zoom: 12
+                });
+            })
+            this.añadirMarker(agencies);
+        }
+    }
+}
 </script>
 
 <style>
