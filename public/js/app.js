3407a413b2a8f80f8510719b94505b757bac5482
ogonzalezz <ogonzalezz27@cepnet.net>
2022-04-28 12:10:28 +0200

    vue

--- public/js/app.js
+++ public/js/app.js
@@ -11595,7 +11595,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n#draggable-container {\r\n  position: absolute;\r\n  z-index: 9;\n}\n#draggable-header {\r\n  z-index: 10;\n}\r\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#draggable-container {\n  position: absolute;\n  z-index: 9;\n}\n#draggable-header {\n  z-index: 10;\n}\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
